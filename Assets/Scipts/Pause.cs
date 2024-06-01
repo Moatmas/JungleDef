9dbee9c8bbd5ec27313e3f64010baf71a4ed45b5
loulouuw <louis.rideau.02@gmail.com>
2024-06-01 22:09:34 +0200

    pause et resume quand on clique sur settings

---
+++ Assets/Scipts/Pause.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI; // Le menu des paramètres
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // Optionnel : Permettre de mettre en pause avec la touche "Escape"
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {Resume();}
+            else
+            {Pause();}
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; // Met le jeu en pause
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; // Reprend le jeu
+        isPaused = false;
+    }
+
+    public void OpenSettings()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+}
